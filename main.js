222908381153485e2a5f20260425763d0c2596fc
nisbusra <busranisanci@outlook.com>
2022-05-08 23:43:41 +0300

    newly added

--- main.js
+++ main.js
@@ -187,21 +187,21 @@
 // ++++++++++++++++++++++++++++++++++
 
 
-// let word = "derya"
-// function check(kelime) {
-//     let result = ""
-//     for (let index = kelime.length-1; index > -1; index--) {        
-//         result+= kelime[index]; 
+let word = "derya"
+function check(kelime) {
+    let result = ""
+    for (let index = kelime.length-1; index > -1; index--) {        
+        result+= kelime[index]; 
               
-//     }
+    }
     
-//     if(kelime === result){
-//         return true;
-//     }else {
-//         return false;
-//     }
-// }
-// console.log(check(word))
+    if(kelime === result){
+        return true;
+    }else {
+        return false;
+    }
+}
+console.log(check(word))
 
 // +++++++++++++++++++++++++++++++++++++
 
@@ -335,19 +335,205 @@
 
 
 
+    
+    
+    
+    // function closestone(sayi) {
+    //     let myArray =[7, 8, 10, 12, 19, 45]
+    
+    //     function bulucu(dizisayilari) {
+            
+    //         for (let index = 0; index < dizisayilari.length; index++) {
+                
+    //             const current = dizisayilari[index];
+    //             for (let secondi = index + 1; secondi < dizisayilari.length; secondi++) {
+    //                 let result = Array
+    //                 const element = dizisayilari[secondi];
+    //                 let toplam = (current + element)
+    //                 if (toplam = num){
+    //                     return "Dizi içerisinde +${toplam}, +${num}'a eşittir." 
+    //                 }else (toplam== num++ || toplam== --num)
+    //                     return "Dizideki +${toplam}$ +{num}'a en yakındır. "
+                    
+    //                 console.log(bulucu(myArray))
+
+
+    //             }
+    //         }
+    //     }console.log(bulucu(myArray))
+//     // }
+//     // console.log(closestone(num))
 
 
 
+// let myArray =[7, 8, 10, 12, 19, 45]
+// let num = 36
+// let myNewArray = [];
+// for (let index = 0; index < myArray.length; index++) {
+                
+//     const current = myArray[index];
+//     for (let secondi = index + 1; secondi < myArray.length; secondi++) {
+        
+//         const element = myArray[secondi];
+//         if(myNewArray.push(element+current)){
+        
+//         }
+        
+//     } 
+// } console.log(myNewArray) 
+// -----------------------------------------
+// function plusMinus(arr= [-4,3,-9,0,4,1]) {
+//     // Write your code here
+    
+//     let zero = 0
+//     let positive =0
+//     let negative = 0;
+    
+//     for(let index=0; index < arr.length; index++){
+//         if(arr[index]<0){
+//             negative++          
+//         }else if(arr[index]>0){
+//             positive++        
+//         }else{
+//             zero++        
+//         }
+    
+//     }
+//     let pporsion = (positive/arr.length).toFixed(6);
+//     let nporsion = (negative/arr.length).toFixed(6);
+//     let nzero = (zero/arr.length).toFixed(6);
+//     console.log(pporsion)
+//     console.log(nporsion)
+//     console.log(nzero)
+    
+// }
+// plusMinus()
 
 
 
+// function fizzBuzz(n) {
+//     let myNewArray = []
+//     for(let i=1; i < n+1; i++){
+//         if((i % 3 === 0) && (i % 5 === 0) ){
+//             myNewArray.push("Fizz");
+//         }else if(i % 5 === 0){
+//             myNewArray.push("Buzz"); 
+//         }else if(i % 3 === 0){
+//             myNewArray.push("FizzBuzz");
+//         }else{
+//             myNewArray.push(String(i));
+//         }
+        
+//     }
+//     return myNewArray
+// };
+// console.log(fizzBuzz(8))
+
+// Bu biçim linear search olmuş. Zaman karmaşası bakımından istediğimizi vermemiş.
 
 
-// parseFloat(string)	converts the numeric floating string to floating-point number
+// var search = function(nums, target) {
+//     for(let index=0; index < nums.length; index++){
+//         if(index = target){
+//             return nums.indexOf(index)
+//         }else{
+//             return -1
+//         }
+//     }
+    
+// };
+// console.log(search([-1,0,3,5,9,12], 9))
+
+// // 0 için çözüm gelmiyor
+// var search = function(nums, target) {
+//     let left = 0, right = nums.length-1
+//     let mid = ((right+left)/2)
+    
+//     if (target < mid){
+//         for(let index=1; index<mid; index++){
+//             if(index = target){
+//                 return nums.indexOf(index)
+//             }else{
+//                 return -1
+//             }
+//          }
+//     }
+//     if(target > mid){
+//         for(let k=mid-1; k < nums.length; k--){
+//             if(k =target){
+//                 return nums.indexOf(k);
+//             }else{
+//                 return -1
+//             }
+//         }
+//     }
+//     if(target === mid){
+//         return mid
+//     }
+    
+// };
+// console.log(search([-1,0,2,5,9,12,-2], 0))
 
 
-// parseInt(string, [radix])	converts the numeric string to integer
+// var search=function(nums, target){
+//     let left=0, right=nums.length -1;
+//     while(left<=right){
+//         let mid = Math.floor(left+right)/2
+//         if(nums[mid] == target){
+//             return mid
+//         }
+//         if(target<nums[mid]){
+//             right = mid-1
+//         }else {
+//             left = mid+1
+//         }
+//     }
+//     return -1
+// }
+// console.log(search([-1,0,2,5,9,12,-2], 0))
 
-// Math floor() function
-// In JavaScript, floor() is a function that is used to return the largest integer value that is less than or equal to a number. In other words, the floor() function rounds a number down and returns an integer value. Because the floor() function is a static function of the Math object, it must be invoked through the placeholder object called Math.
-// Math.floor(number);
+
+// var solution = function(isBadVersion) {
+//     /**
+//      * @param {integer} n Total versions
+//      * @return {integer} The first bad version
+//      */
+//     return function(n) {
+//         let left = 1, right = n;
+//         while(left<=right){
+//             let mid = Math.floor(left + (right - left)/2);
+            
+//             if(isBadVersion(mid)){
+//                 right = mid-1
+//             }else{
+//                 left = mid+1
+//             }
+            
+//         }
+        
+//         return left
+//     }
+// }
+
+// console.log(5)
+
+// function staircase(n) {
+//     // Write your code here
+    
+//     for( let index=1; index < n+1; index++){
+//       const element = "#".repeat(index+1)
+//       element.padStart(index)
+//     }
+    
+// }
+// staircase(6)
+
+
+function staircase(n) {
+    // Write your code here
+    
+    for( let index=1; index < n+1; index++){
+        
+        console.log("#".repeat(index).padStart(n))
+    }
+}
